f88a3409ae910293d233c5aafc348ddf3f2b120c
Secaly <secaly@Secalys-MacBook-Pro.local>
2018-12-04 17:42:04 +0100

    adding server

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(8080, () => console.log('Running on localhost:8080'));
