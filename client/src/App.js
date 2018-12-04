5607bf5e72e9f78f33e020e479c118d5b527f637
Secaly <secaly@Secalys-MacBook-Pro.local>
2018-12-04 15:46:59 +0100

    adding yarn workspaces

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GamePage from './components/GamePage';
+
+const App = () => (
+  <div>
+    Hello World
+    <GamePage />
+  </div>
+);
+
+export default App;
---
+++ client/src/components/GamePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GamePage = () => <div>GamePage</div>;
+
+export default GamePage;
