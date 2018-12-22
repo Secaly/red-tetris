249ebc1137b32ead89306e79209a89a44f1dab17
Secaly <kevin.ledieu.fr@gmail.com>
2018-12-22 16:26:08 +0100

    adding more game logic

--- client/src/reducers/game.js
+++ client/src/reducers/game.js
@@ -54,6 +54,9 @@ const joinPiece = game => {
   _.forEach(game.piece.form[game.piece.rotation], (line, lineIndex) =>
     _.forEach(line, (cell, cellIndex) => {
       if (
+        game.piece.pos[0] + lineIndex < 20 &&
+        game.piece.pos[1] + cellIndex >= 0 &&
+        game.piece.pos[1] + cellIndex < 10 &&
         game.boardFlex[game.piece.pos[0] + lineIndex][
           game.piece.pos[1] + cellIndex
         ] === 0
@@ -70,6 +73,9 @@ const fixPiece = game => {
   _.forEach(game.piece.form[game.piece.rotation], (line, lineIndex) =>
     _.forEach(line, (cell, cellIndex) => {
       if (
+        game.piece.pos[0] + lineIndex < 20 &&
+        game.piece.pos[1] + cellIndex >= 0 &&
+        game.piece.pos[1] + cellIndex < 10 &&
         game.boardFix[game.piece.pos[0] + lineIndex][
           game.piece.pos[1] + cellIndex
         ] === 0
@@ -88,37 +94,37 @@ const isFree = (game, movement) => {
     case 'left':
       return !game.piece.form[game.piece.rotation].some((line, lineIndex) =>
         line.some(
-          (cell, caseIndex) =>
-            ((game.piece.pos[1] + caseIndex > 0 &&
+          (cell, cellIndex) =>
+            cell === 1 &&
+            ((game.piece.pos[1] + cellIndex - 1 >= 0 &&
               game.boardFix[game.piece.pos[0] + lineIndex][
-                game.piece.pos[1] + caseIndex
+                game.piece.pos[1] + cellIndex - 1
               ] === 1) ||
-              game.piece.pos[1] + caseIndex === 0) &&
-            cell === 1,
+              game.piece.pos[1] + cellIndex - 1 < 0),
         ),
       );
     case 'right':
       return !game.piece.form[game.piece.rotation].some((line, lineIndex) =>
         line.some(
-          (cell, caseIndex) =>
-            ((game.piece.pos[1] + caseIndex + 1 < 10 &&
+          (cell, cellIndex) =>
+            cell === 1 &&
+            ((game.piece.pos[1] + cellIndex + 1 < 10 &&
               game.boardFix[game.piece.pos[0] + lineIndex][
-                game.piece.pos[1] + caseIndex + 1
+                game.piece.pos[1] + cellIndex + 1
               ] === 1) ||
-              game.piece.pos[1] + caseIndex + 1 === 10) &&
-            cell === 1,
+              game.piece.pos[1] + cellIndex + 1 === 10),
         ),
       );
     case 'down':
       return !game.piece.form[game.piece.rotation].some((line, lineIndex) =>
         line.some(
-          (cell, caseIndex) =>
+          (cell, cellIndex) =>
+            cell === 1 &&
             ((game.piece.pos[0] + lineIndex + 1 < 20 &&
               game.boardFix[game.piece.pos[0] + lineIndex + 1][
-                game.piece.pos[1] + caseIndex
+                game.piece.pos[1] + cellIndex
               ] === 1) ||
-              game.piece.pos[0] + lineIndex + 1 === 20) &&
-            cell === 1,
+              game.piece.pos[0] + lineIndex + 1 === 20),
         ),
       );
     default:
@@ -142,12 +148,52 @@ const moveRight = game => {
   return { ...game };
 };
 
+const removeFullLines = game => {
+  _.forEach(game.boardFix, (line, lineIndex) => {
+    if (line.every(cell => cell === 1)) {
+      game.boardFix.splice(lineIndex, 1);
+      game.boardFix.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+  });
+};
+
 const moveDown = game => {
   if (game.piece && isFree(game, 'down')) {
     game.piece.pos[0] += 1;
     joinPiece(game);
   } else if (game.piece && !isFree(game, 'down')) {
     fixPiece(game);
+    removeFullLines(game);
+  }
+  return { ...game };
+};
+
+const canRotate = (game, rotation) =>
+  !rotation.some((line, lineIndex) =>
+    line.some(
+      (cell, cellIndex) =>
+        (cell === 1 &&
+          game.boardFix[game.piece.pos[0] + lineIndex][
+            game.piece.pos[1] + cellIndex
+          ] === 1) ||
+        game.piece.pos[0] + lineIndex >= 20 ||
+        game.piece.pos[1] + cellIndex < 0 ||
+        game.piece.pos[1] + cellIndex >= 10,
+    ),
+  );
+
+const rotateLeft = game => {
+  if (canRotate(game, game.piece.form[(game.piece.rotation + 3) % 4])) {
+    game.piece.rotation = (game.piece.rotation + 3) % 4;
+    joinPiece(game);
+  }
+  return { ...game };
+};
+
+const rotateRight = game => {
+  if (canRotate(game, game.piece.form[(game.piece.rotation + 5) % 4])) {
+    game.piece.rotation = (game.piece.rotation + 5) % 4;
+    joinPiece(game);
   }
   return { ...game };
 };
@@ -155,10 +201,10 @@ const moveDown = game => {
 const getPiece = game => {
   game.piece = {
     form: {
-      0: [[0, 0, 0], [1, 1, 0], [0, 1, 0], [0, 1, 0]],
-      1: [[0, 0, 0], [0, 1, 0], [0, 1, 1], [0, 1, 0]],
-      2: [[0, 0, 0], [0, 0, 0], [1, 1, 1], [0, 1, 0]],
-      3: [[0, 0, 0], [0, 1, 0], [1, 1, 0], [0, 1, 0]],
+      0: [[0, 0, 1], [1, 1, 1], [0, 0, 0]],
+      1: [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
+      2: [[0, 0, 0], [1, 1, 1], [1, 0, 0]],
+      3: [[1, 1, 0], [0, 1, 0], [0, 1, 0]],
     },
     rotation: 0,
     pos: [0, 2],
@@ -183,6 +229,12 @@ export default function game(state = initialState, action = {}) {
         }
         case 40: {
           return moveDown(state);
+        }
+        case 65: {
+          return rotateLeft(state);
+        }
+        case 83: {
+          return rotateRight(state);
         }
         default:
           return state;
